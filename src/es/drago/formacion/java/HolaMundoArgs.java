9cb202ab3c5eb9add7b6b4d4e285fb258d6d4a17
fjperezc <fjperezc@visionitgroup.es>
2015-03-09 16:13:58 +0100

    hola mundo "

---
+++ src/es/drago/formacion/java/HolaMundoArgs.java
@@ -0,0 +1,14 @@
+package es.drago.formacion.java;
+
+public class HolaMundoArgs {
+
+	public static void main(String[] args) {
+
+		if ( args.length != 0 ) {
+			System.out.println("Hola " + args[0]);
+		}
+			
+
+	}
+
+}
---
+++ src/es/drago/formacion/java/HolaMundoArgsCheck.java
@@ -0,0 +1,11 @@
+package es.drago.formacion.java;
+
+public class HolaMundoArgsCheck {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola " + args[0]);
+
+	}
+
+}
